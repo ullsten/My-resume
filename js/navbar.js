86a640cacaff0fa8a30d4e86b03b3e18e385bafc
ullsten <113597614+ullsten@users.noreply.github.com>
2023-02-09 21:36:25 +0100

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 // script that adds navbar to every page from navbar.html
 
-fetch('/Labb1_SkapaCV_Sida/html/navbar.html')
+fetch('navbar.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#add_navbar");
