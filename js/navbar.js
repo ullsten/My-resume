f13a96040a08d99269b5e4771069b8a2f83d9410
ullsten <113597614+ullsten@users.noreply.github.com>
2023-02-09 21:42:37 +0100

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 // script that adds navbar to every page from navbar.html
 
-fetch('navbar.html')
+fetch('./navbar.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#add_navbar");
